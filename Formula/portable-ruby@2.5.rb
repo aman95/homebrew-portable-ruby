5e795c563d199adf78e891c905c812c6aa1dc20a
Aman Kumar <aman1995k@gmail.com>
2020-04-26 16:52:49 +0530

    Create portable-ruby@2.5.rb

---
+++ Formula/portable-ruby@2.5.rb
@@ -0,0 +1,113 @@
+require File.expand_path("../Abstract/portable-formula", __dir__)
+
+class PortableRubyAT25 < PortableFormula
+  desc "Powerful, clean, object-oriented scripting language"
+  homepage "https://www.ruby-lang.org/"
+  # Ruby 2.5.8 version
+  url "https://cache.ruby-lang.org/pub/ruby/2.5/ruby-2.5.8.tar.xz"
+  mirror "https://cache.ruby-lang.org/pub/ruby/2.5/ruby-2.5.8.tar.xz"
+  sha256 "0391b2ffad3133e274469f9953ebfd0c9f7c186238968cbdeeb0651aa02a4d6d"
+
+  bottle do
+    cellar :any_skip_relocation
+    # sha256 "ab81211a2052ccaa6d050741c433b728d0641523d8742eef23a5b450811e5104" => :mavericks
+    sha256 "e8c9b6d3dc5f40844e07b4b694897b8b7cb5a7dab1013b3b8712a22868f98c98" => :x86_64_linux
+  end
+
+  depends_on "makedepend" => :build
+  depends_on "pkg-config" => :build
+  depends_on "portable-readline" => :build
+  depends_on "portable-libyaml" => :build
+  depends_on "portable-openssl" => :build
+  if OS.linux?
+    depends_on "portable-ncurses" => :build
+    depends_on "portable-zlib" => :build
+  end
+
+  def install
+    readline = Formula["portable-readline"]
+    libyaml = Formula["portable-libyaml"]
+    openssl = Formula["portable-openssl"]
+    ncurses = Formula["portable-ncurses"]
+    zlib = Formula["portable-zlib"]
+
+    args = %W[
+      --prefix=#{prefix}
+      --enable-load-relative
+      --with-static-linked-ext
+      --with-out-ext=tk,sdbm,gdbm,dbm
+      --without-gmp
+      --disable-install-doc
+      --disable-install-rdoc
+      --disable-dependency-tracking
+    ]
+
+    paths = [
+      readline.opt_prefix,
+      libyaml.opt_prefix,
+      openssl.opt_prefix,
+    ]
+
+    if OS.linux?
+      # We want Ruby to link to our ncurses, instead of libtermcap in CentOS 5
+      paths << ncurses.opt_prefix
+      inreplace "ext/readline/extconf.rb" do |s|
+        s.gsub! "dir_config('termcap')", ""
+        s.gsub! 'have_library("termcap", "tgetnum") ||', ""
+      end
+
+      paths << zlib.opt_prefix
+    end
+
+    args << "--with-opt-dir=#{paths.join(":")}"
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+
+    # rake is a binstub for the RubyGem in 2.3 and has a hardcoded PATH.
+    # We don't need the binstub so remove it.
+    rm bin/"rake"
+
+    abi_version = `#{bin}/ruby -rrbconfig -e 'print RbConfig::CONFIG["ruby_version"]'`
+    abi_arch = `#{bin}/ruby -rrbconfig -e 'print RbConfig::CONFIG["arch"]'`
+    inreplace lib/"ruby/#{abi_version}/#{abi_arch}/rbconfig.rb" do |s|
+      s.gsub! ENV.cxx, "c++"
+      s.gsub! ENV.cc, "cc"
+    end
+
+    libexec.mkpath
+    cp openssl.opt_libexec/"etc/openssl/cert.pem", libexec/"cert.pem"
+    openssl_rb = lib/"ruby/#{abi_version}/openssl.rb"
+    openssl_rb_content = openssl_rb.read
+    rm openssl_rb
+    openssl_rb.write <<~EOS
+      ENV["SSL_CERT_FILE"] ||= File.expand_path("../../libexec/cert.pem", RbConfig.ruby)
+      #{openssl_rb_content}
+    EOS
+  end
+
+  test do
+    cp_r Dir["#{prefix}/*"], testpath
+    ENV["PATH"] = "/usr/bin:/bin"
+    ruby = (testpath/"bin/ruby").realpath
+    assert_equal version.to_s.split("-").first, shell_output("#{ruby} -e 'puts RUBY_VERSION'").strip
+    assert_equal ruby.to_s, shell_output("#{ruby} -e 'puts RbConfig.ruby'").strip
+    assert_equal "3632233996",
+      shell_output("#{ruby} -rzlib -e 'puts Zlib.crc32(\"test\")'").strip
+    assert_equal "\"'",
+      shell_output("#{ruby} -rreadline -e 'puts Readline.basic_quote_characters'").strip
+    assert_equal '{"a"=>"b"}',
+      shell_output("#{ruby} -ryaml -e 'puts YAML.load(\"a: b\")'").strip
+    assert_equal "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+      shell_output("#{ruby} -ropenssl -e 'puts OpenSSL::Digest::SHA256.hexdigest(\"\")'").strip
+    assert_match "200",
+      shell_output("#{ruby} -ropen-uri -e 'open(\"https://google.com\") { |f| puts f.status.first }'").strip
+    system testpath/"bin/gem", "environment"
+    system testpath/"bin/bundle", "init"
+    # install gem with native components
+    system testpath/"bin/gem", "install", "byebug"
+    assert_match "byebug",
+      shell_output("#{testpath}/bin/byebug --version")
+  end
+end
